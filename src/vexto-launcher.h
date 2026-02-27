c818984327e6315fb1f31bffc386619cf402a8ad
Thiago Aciole <thiagoaciole7@gmail.com>
2026-02-27 17:38:47 -0300

    feat: Add Xfce Panel plugin desktop entry and .gitignore for initial project setup.

--- src/vexto-launcher.h
+++ src/vexto-launcher.h
@@ -3,8 +3,8 @@
 
 #include <gtk/gtk.h>
 #include <libxfce4panel/libxfce4panel.h>
-#include <libxfce4util/libxfce4util.h>
 #include <libxfce4ui/libxfce4ui.h>
+#include <libxfce4util/libxfce4util.h>
 
 /* Constants */
 #define DEFAULT_WINDOW_WIDTH 600
@@ -14,25 +14,25 @@
 #define GRID_ROWS 3
 
 typedef struct {
-    XfcePanelPlugin *plugin;
-    
-    /* UI Components */
-    GtkWidget       *button;        /* Panel button */
-    GtkWidget       *icon;          /* Panel icon */
-    GtkWidget       *window;        /* Main launcher popup */
-    GtkWidget       *header;        /* Search area */
-    GtkWidget       *grid;          /* App grid */
-    GtkWidget       *search_entry;  /* Search input */
-    GtkWidget       *page_dots;     /* Pagination container */
-    GtkWidget       *footer;        /* Bottom actions area */
-    
-    /* Data */
-    GList           *all_apps;
-    GList           *filtered_apps; /* Currently filtered set */
-    gint            current_page;
-    gint            total_pages;
-    gint            icon_size;      /* Custom icon size */
-    gchar           *icon_name;     /* Custom icon name */
+  XfcePanelPlugin *plugin;
+
+  /* UI Components */
+  GtkWidget *button;       /* Panel button */
+  GtkWidget *icon;         /* Panel icon */
+  GtkWidget *window;       /* Main launcher popup */
+  GtkWidget *header;       /* Search area */
+  GtkWidget *grid;         /* App grid */
+  GtkWidget *search_entry; /* Search input */
+  GtkWidget *page_dots;    /* Pagination container */
+  GtkWidget *footer;       /* Bottom actions area */
+
+  /* Data */
+  GList *all_apps;
+  GList *filtered_apps; /* Currently filtered set */
+  gint current_page;
+  gint total_pages;
+  gint icon_size;   /* Custom icon size */
+  gchar *icon_name; /* Custom icon name */
 } VextoLauncher;
 
 /* Component functions */
